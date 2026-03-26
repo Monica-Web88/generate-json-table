9a95dda020a18927eb608879ac0124315b5ce4f1
Monica-Web88 <itsme.cloudy3@gmail.com>
2026-03-26 12:59:24 -0700

    router changes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from "react";
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./styles.css";
 
 export default function App() {
   return (
     <div>
-      
-        <Routes>
+    
+       <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
-    
+  
     </div>
   );
 }
